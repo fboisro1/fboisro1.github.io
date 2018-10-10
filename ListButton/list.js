f7e137ba575acfc8d597b192c7a2a11185031c3d
Sefu Kaba <frautz.boisrond@bellevuecollege.edu>
2018-10-10 14:06:25 -0700

    ListButton

---
+++ ListButton/list.js
@@ -0,0 +1,16 @@
+window.onload = () =>{
+    const setTextButton = document.getElementById('set-text');
+    const userInput = document.getElementById('list-data');
+    const listButton = document.getElementById('add-to-list'); 
+    
+    listButton.onclick = ()=>{
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
+    }
+    setTextButton.onclick = () => {
+        userInput.value = 'The First Item for my List';
+        
+        }
+    }
+    
+
