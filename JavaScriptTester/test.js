5bc7bca70436dba2cff24450b73d5ce259263fc5
Sefu Kaba <frautz.boisrond@bellevuecollege.edu>
2018-10-10 11:29:42 -0700

    I did this today

--- JavaScriptTester/test.js
+++
@@ -1,7 +0,0 @@
-var Prog109 ='Prog on Monday';
-var a = 10;
-var b = 15;
-
-console.log(a + b);
-console.log('Prog109');
-console.log('a + b =', a+b);
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fboisro1.github.io
 This is my repository for Prog109
+I wrote this in class
