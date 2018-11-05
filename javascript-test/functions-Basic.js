0ae175492772cc7567ae9e2a35f08f5e80e30a03
Sefu Kaba <frautz.boisrond@bellevuecollege.edu>
2018-11-05 12:17:18 -0800

    FunctionBasics

---
+++ javascript-test/functions-Basic.js
@@ -0,0 +1,20 @@
+function simpleFunction() {
+    console.log("Functions should consist of statements designed to perform a single task.")
+}
+function add(a,b){
+    console.log(a+b);
+}
+
+function adder (){
+    console.log(2+3);
+}
+
+function functionParameters(value){
+    console.log(value)
+}
+
+simpleFunction();
+functionParameters("Many functions take parameters.");
+add(2,3);
+//add(4,9);
+adder();
