e2b070268867ee764e4ea9e7e982b3bd9ade347d
Sefu Kaba <frautz.boisrond@bellevuecollege.edu>
2018-11-08 20:20:43 -0800

    useful-links

--- For-Loops/index.js
+++ For-Loops/index.js
@@ -1,15 +1,20 @@
 window.onload = () => {
+
     const listButton = document.getElementById('run-for-loop');
+    /*getElementById calls a specific element by name, example "I want to grab *example*
+    and I want it to do .... " it returns a value and we save it in the variable list button. */
 
-    listButton.onclick = () => {
-        const myList = document.getElementById('my-list');
-            // WRAP THE NEXT LINE IN A for-loop that iterates over a variable named i. 
-            // The loop should iterate 5 times (count from zero to five).
 
-            for ( let i = 0; i <= 5; i = i + 1){
-                ;
+
+    listButton.onclick = function () {
+        const myList = document.getElementById('my-List');
+        // WRAP THE NEXT LINE IN A for-loop that iterates over a variable named i. 
+        // The loop should iterate 5 times (count from zero to five).
+        // i++ =,  i = i + 1
+        //for ( let i = 0; i <= 5; i = i + 1){
+        for (let i = 0; i < 5; i++) {
             elfCode.appendToList(myList, i);
-            }
-            // END LOOP
+        }
+        // END LOOP
     }
 }  
