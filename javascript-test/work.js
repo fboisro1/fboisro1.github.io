df1995d97782521c86f47befe86ff154bf2124c3
Sefu Kaba <frautz.boisrond@bellevuecollege.edu>
2018-11-17 16:36:18 -0800

    Javascript Objects

--- javascript-test/work.js
+++ javascript-test/work.js
@@ -1,7 +1,70 @@
 
-function person(Sefu, Kaba){
-    console.log();
+/*const person = {};
+person.firstName = 'Sefu';
+person.lastName = 'Kaba';
+person.fullName = person.firstName + ' ' + person.lastName;
+
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person.fullName)
+
+const calculator = {};*/
+function divider(title) {
+    console.log("====================================");
+    console.log(title);
+    console.log("====================================");
 }
-   
+
+//Person 
+const person = {
+    firstName: 'Sefu',
+    lastName: 'Kaba',
+    fullName: function() {'use strict';
+        return this.firstName + ' ' + this.lastName;
+    }
+};
+
+//Calculator
+
+const calculator = {
+    operand01: 4,
+    operand02: 4,
+    add: function(){
+        'use strict';
+        return this.operand01 + this.operand02;
+        
+    },
+    
+    multiply: function() {
+        'use strict';
+        return this.operand01 * this.operand02;
+    },
+
+    divide: function(){
+        'use strict';
+        return this.operand01 / this.operand02;
+    }
+
+    
+};
 
 
+calculator.operand01 = person.firstName.length;
+calculator.operand02 = person.lastName.length;
+
+
+divider('Person');
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName());
+divider('Calculator');
+console.log('operand01 =', calculator.operand01);
+console.log('operand01 =', calculator.operand02);
+console.log('Add =', calculator.add());
+console.log('operand01 =', calculator.multiply());
+console.log('operand01 =', calculator.divide());
+//console.log(calculator.operand01);
+//console.log(calculator.operand02);
+//console.log(calculator.add());
+//console.log(calculator.multiply());
+//console.log(calculator.divide());
